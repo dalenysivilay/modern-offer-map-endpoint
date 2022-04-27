6f00491955212d68c492c933ad3e5e79e981aaeb
dalenysivilay <dalenysivilay@gmail.com>
2022-04-27 16:23:02 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import { Router } from 'itty-router';
+
+const router = Router();
+
+router.get('/', () => new Response('Google Map'));
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+/**
+ * Respond with hello worker text
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+  return new Response('Hello worker!', {
+    headers: { 'content-type': 'text/plain' },
+  })
+}
